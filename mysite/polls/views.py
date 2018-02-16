ba21b123eb6b5b48fb63e5fc2e7829318ac7a781
devanshugarg1994 <devanshugarg1994@gmail.com>
2018-02-16 21:54:20 +0530

    Poll application upto 3rd Tutorial

---
+++ mysite/polls/views.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse, Http404
+from django.shortcuts import render
+
+from .models import Question
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+	'latest_question_list' : latest_question_list,
+    }
+    return render(request, 'polls/index.html',context)
+
+
+def results(request, question_id):
+	response = "You are looking at the results of question %s"
+	return HttpResponse("YOU ARE    looking at the results of question %s" % question_id)
+
+def detail(request, question_id):
+	try:
+		question = Question.objects.get(pk=question_id)
+	except Question.DoesNotExist:
+		raise Http404("Question does not exist")
+	return render(request ,'polls/detail.html' , {'question' : question})
+
+def vote(request, question_id):
+	return HttpResponse("You are voting on question %s" % question_id)
